124c212d84f8c16db2c274e2fad24b8770d04709
NaVteJSingH18 <navtej.dgc@gmail.com>
2026-02-22 23:37:26 +0530

    Initial commit

---
+++ port/script.js
@@ -0,0 +1,5 @@
+// document.getElementsByClassName("nav-links")[0].addEventListener("click", function(event) {
+//     if (event.target.tagName === "A") {
+//         event.target.style.backgroundColor = "oklch(90.598% 0.01091 286.047 / 0.08)";
+//     }
+// })

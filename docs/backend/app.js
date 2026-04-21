990d4d39691e68b7a5991afb295f0de11d9e6db9
NaVteJSingH18 <navtej.dgc@gmail.com>
2026-04-21 23:32:45 +0530

    blogs front end

--- docs/backend/app.js
+++ docs/backend/app.js
@@ -4,6 +4,11 @@ const mongoose = require("mongoose")
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+const cors = require("cors");
+
+app.use(cors({
+  origin: "http://127.0.0.1:5500"
+}));
 const blogRoutes = require("./routes/blog.routes")
 
 app.use("/api/blogs",blogRoutes);
--- docs/backend/controllers/blog.controller.js
+++ docs/backend/controllers/blog.controller.js
@@ -1,34 +1,62 @@
-const Blog = require("../models/blog.model")
+const Blog = require("../models/blog.model");
 
-exports.createBlog = async ( req,res)=>{
-    try{
-        const {title,image,content} = req.body;
+exports.createBlog = async (req, res) => {
+  try {
+    const { title, image, content } = req.body;
 
-        const slug = title.toLowerCase().replace(/ /g,"-")
-
-        const blog = await Blog.create({
-            title,
-            slug,
-            content,
-            image
-        })
-        res.status(200).json(blog);
-
-
+    if (!title || !content) {
+      return res.status(400).json({ message: "Title and content are required" });
     }
-    catch(err){
-            res.status(500).json({ error: err.message });
+
+    let slug = title
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-");
+
+    let baseSlug = slug;
+    let counter = 1;
+
+    while (await Blog.findOne({ slug })) {
+      slug = `${baseSlug}-${counter++}`;
     }
+
+    const blog = await Blog.create({
+      title,
+      slug,
+      content,
+      image,
+    });
+
+    res.status(201).json(blog);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 // all blogs
-exports.getBlogs =  async (req,res)=>{
-    const blogs = await Blog.find().sort({createdAt:-1});
-    res.json(blogs)
-}
+exports.getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-//single blog
-exports.getBlog = async (req,res)=>{
-    const blog = await Blog.findOne({ slug:req.params.slug});
-    res.json(blog)
-}
+// single blog
+exports.getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findOne({ slug: req.params.slug });
+
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.json(blog);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
